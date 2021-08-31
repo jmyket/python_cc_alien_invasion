c02e892295d055e3b78d4ff336ed900420589d98
Josh Myket <josh.myket@gmail.com>
2021-08-30 20:31:06 -0400

    Initial commit of code files.

---
+++ alien_invasion.py
@@ -0,0 +1,32 @@
+import sys
+import pygame
+from settings import Settings
+
+class AlienInvasion:
+    """Overall class to manage game assets and behavior."""
+
+    def __init__(self):
+        """Initialize the game, and create game resources."""
+        pygame.init()
+        self.settings = Settings()
+        self.screen = pygame.display.set_mode(
+            (self.settings.screen_width, self.settings.screen_height)
+        )
+        pygame.display.set_caption("Alien Invasion")
+
+    def run_game(self):
+        """Start the main loop for the game."""
+        while True:
+            # WATCH FOR KB AND MOUSE EVENTS
+            for event in pygame.event.get():
+                if event.type == pygame.QUIT:
+                    sys.exit()
+            
+            self.screen.fill(self.settings.bg_color)
+            
+            # MAKE THE MOST RECENTLY DRAWN SCREEN VISIBLE
+            pygame.display.flip()
+
+if __name__ == '__main__':
+    ai = AlienInvasion()
+    ai.run_game()
